47631363d1e32e7a657d0ff249a7490ba7c578af
Magnus Westhofen <m.westhofen@12-05.de>
2021-12-20 15:09:50 +0100

    Update blog-preview.php

--- blocks/blog-preview.php
+++ blocks/blog-preview.php
@@ -13,7 +13,7 @@
                 <div class="content">
                     <span class="date"><?php echo get_the_date('d.m.Y', $post->ID);?></span>
                     <h3><?php echo get_the_title($post->ID);?></h3>
-                    <p><?php echo get_the_excerpt($post->ID);?></p>
+                    <p><?php echo wpautop(get_the_excerpt($post->ID));?></p>
 
         
                 </div>
