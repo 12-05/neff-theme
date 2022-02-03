e8e024dbda3e04d8776cc06ea85f563f3fcc3c81
Magnus Westhofen <m.westhofen@12-05.de>
2022-02-03 14:47:46 +0100

    test

--- blocks/video.php
+++ blocks/video.php
@@ -1,5 +1,8 @@
 <section class="block block-video">
 <video autoplay="" loop="" muted="" playsinline="" src="<?php the_field('video');?>">
 </video>
+    <?php if(get_field('headline')) {?>
+        <div class="headline"><?php the_field('headline');?></div>
+    <?php } ?> 
     <a target="_blank" href="<?php the_field('link');?>"><img src="<?php echo get_template_directory_uri().'/assets/images/play.svg';?>" alt="Play" /></a>
 </section>
