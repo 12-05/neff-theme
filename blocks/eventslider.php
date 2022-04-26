c5880685145e03521787d765d9fb35d52a9a1e26
Magnus Westhofen <m.westhofen@12-05.de>
2022-04-26 15:15:28 +0200

    test

--- blocks/eventslider.php
+++ blocks/eventslider.php
@@ -127,7 +127,6 @@ jQuery(document).ready( function($) {
             slidesToShow: 3.5,
             slidesToScroll: 1,
             infinite: false,
-            dots: true
           }
         },
         {
