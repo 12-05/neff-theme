6af761a7166ef55a5fade95d682ed687cb8298ad
Magnus Westhofen <m.westhofen@12-05.de>
2022-08-03 14:16:38 +0200

    test

--- page-partnerformular.php
+++ page-partnerformular.php
@@ -23,6 +23,7 @@
         'submit_value'  => 'Neuen Partner erstellen'
     )); ?>
     <?php acf_enqueue_uploader();?>
+    <p>Bei technischen Schwierigkeiten oder Rückfragen melden Sie sich bitte unter spanke@neue-effizienz.de.</p>
  </div>
 <?php } ?>
  
