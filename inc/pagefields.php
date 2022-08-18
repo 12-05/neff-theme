b50ad5f29f07e05d30d53858020d3e2382f677ee
Magnus Westhofen <m.westhofen@12-05.de>
2022-08-18 18:22:06 +0200

    test

--- inc/pagefields.php
+++ inc/pagefields.php
@@ -50,10 +50,3 @@ if( function_exists('acf_add_local_field_group') ):
 
     add_filter( 'use_block_editor_for_post', 'my_disable_gutenberg', 10, 2 );
 
-function my_disable_gutenberg( $can_edit, $post ) {
-  if( $post->post_type == 'page' &&
-    get_page_template_slug( $post->ID ) == 'page-shortcode.php' ) {
-    return false;}
-
-  return $can_edit;
-}
