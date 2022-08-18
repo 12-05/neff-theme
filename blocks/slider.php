b04ea3892ca55520869b2e41ec6e905013a9265d
Magnus Westhofen <m.westhofen@12-05.de>
2022-08-18 18:31:35 +0200

    text

--- blocks/slider.php
+++ blocks/slider.php
@@ -2,7 +2,7 @@
 <section class="block block-slider">
     <?php if($slides):foreach($slides as $slide):?>
         <?php if($slide['link']) {?>
-          <a href="<?php echo $slide['link'];?>" class="slide position-<?php echo $slide['content_position'];?>" style="background-image:url(<?php echo $slide['bild'];?>);background-size:<?php echo $slide['format'];?> !important;background-repeat:no-repeat;">
+          <a href="<?php echo $slide['link'];?>" class="slide position-<?php echo $slide['content_position'];?>" style="text-decoration:none;background-image:url(<?php echo $slide['bild'];?>);background-size:<?php echo $slide['format'];?> !important;background-repeat:no-repeat;">
             <div class="content">
                 <?php if($slide['logo']) {?>
                      <img src="<?php echo $slide['logo'];?>" />
