325357eac6384d6b795ce7c28124d653d98c96fb
Magnus Westhofen <m.westhofen@12-05.de>
2023-01-09 13:16:33 +0100

    test

--- blocks/youtube.php
+++ blocks/youtube.php
@@ -4,6 +4,8 @@
     $headline = get_field('headline');
 ?>
 <section class="block block-youtube <?php if(get_field('hintergrundfarbe')) {echo 'youtube-bg';}?>" style="background-color:<?php echo $farbe;?>">
-   <?php if($headline) {?><h2 class="headline"><?php echo get_field('headline');?></h2><?php } ?>
-    <div class="inner"><iframe src="https://www.youtube.com/embed/<?php echo $id;?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></div>
+    <div class="inner">
+        <?php if($headline) {?><h2 class="headline"><?php echo get_field('headline');?></h2><?php } ?>
+
+        <iframe src="https://www.youtube.com/embed/<?php echo $id;?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></div>
 </section>
