1c0f4392b7836989cf30164e7b956e7da29c7152
Magnus Westhofen <m.westhofen@12-05.de>
2023-01-09 13:20:35 +0100

    kontakt-color

--- blocks/kontaktformular.php
+++ blocks/kontaktformular.php
@@ -1,4 +1,5 @@
-<section class="block-kontaktformular">
+<?php $color = get_field('hintergrundfarbe');?>
+<section class="block-kontaktformular <?php if($color) {?>kontakt-bg<?php }?>" <?php if($color) {?> style="background-color: <?php echo $color;?>"<?php } ?>>
     <span class="subline"><?php the_field('subline');?></span>
     <h2 class="headline"><?php the_field('headline');?></h2>
     <?php echo do_shortcode('[contact-form-7 id="'.get_field('formular').'" title="Veranstaltungen"]');?>
