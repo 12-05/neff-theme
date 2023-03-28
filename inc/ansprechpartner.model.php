4b5f514555a5c391e4056701945984a43a5c7e07
Magnus Westhofen <m.westhofen@12-05.de>
2023-03-28 10:14:40 +0200

    test

--- inc/ansprechpartner.model.php
+++ inc/ansprechpartner.model.php
@@ -76,7 +76,7 @@
              URL;CHARSET=UTF-8;type=WORK:https://neue-effizienz.de
              END:VCARD';
              $vcard_file = 'neue-effizienz-'.get_the_title().'.vcf';                     
-            echo '<a href="data:text/vcard;charset=utf-8,'.rawurlencode($vcard).'" download="'.$vcard_file.'" class="download-button">';
+            echo '<a href="data:text/x-vcard;base64,'.base64_encode($vcard).'" download="'.$vcard_file.'" class="download-button">';
             echo '<span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#666" d="M20 4H4c-1.11 0-1.99.89-1.99 2L2 18c0 1.11.89 2 2 2h16c1.11 0 2-.89 2-2V6c0-1.11-.89-2-2-2zm0 14H4v-6h16v6zm0-10H4V6h16v2z"/></svg></span>';
             echo '</a>';
         }
