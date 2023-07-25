7bcb8d83eceabd5bd46928b71e8e609ce7fe5a64
Magnus Westhofen <m.westhofen@12-05.de>
2023-07-25 17:32:36 +0200

    test

--- blocks/projektloop.php
+++ blocks/projektloop.php
@@ -10,15 +10,15 @@ if (get_field('projektauswahl')) {
 if ($projekte): foreach ($projekte as $projekt): ?>
 
 
-	        <a target="<?php the_field('linkziel');?>" href="<?php echo get_field('externer_link', $projekt->ID) ? get_field('externer_link', $projekt->ID) : get_permalink($projekt->ID); ?>"class="projekt">
-	            <div class="bild" style="background-image:url(<?php echo get_field('bild', $projekt->ID); ?>"></div>
-	            <div class="content">
-	                <div style="color:<?php echo getProjectColor($projekt->ID); ?>" class="subline">Projekt</div>
-	                <div class="headline"><?php echo $projekt->post_title; ?></div>
-	                <div class="description"><?php the_field('kurzbeschreibung', $projekt->ID);?></div>
-	                <span style="background:<?php echo getProjectColor($projekt->ID); ?>" class="link"></span>
-	            </div>
-	        </a>
-	        <?php endforeach;endif;
+			        <a target="<?php echo get_field('externer_link', $projekt->ID) ? "_blank" : "_self"; ?>" href="<?php echo get_field('externer_link', $projekt->ID) ? get_field('externer_link', $projekt->ID) : get_permalink($projekt->ID); ?>"class="projekt">
+			            <div class="bild" style="background-image:url(<?php echo get_field('bild', $projekt->ID); ?>"></div>
+			            <div class="content">
+			                <div style="color:<?php echo getProjectColor($projekt->ID); ?>" class="subline">Projekt</div>
+			                <div class="headline"><?php echo $projekt->post_title; ?></div>
+			                <div class="description"><?php the_field('kurzbeschreibung', $projekt->ID);?></div>
+			                <span style="background:<?php echo getProjectColor($projekt->ID); ?>" class="link"></span>
+			            </div>
+			        </a>
+			        <?php endforeach;endif;
 ?>
 </section>
