ba96313ccb4afb752c1f5879e481ae9a6a7b5166
Magnus Westhofen <m.westhofen@12-05.de>
2023-07-25 17:30:40 +0200

    test

--- blocks/projektloop.php
+++ blocks/projektloop.php
@@ -1,24 +1,24 @@
 <section class="block block-projektloop">
-    <?php 
-        $projekte = get_posts(array(
-            'post_type' => "projekt", 
-            'posts_per_page' => -1,
-        ));
-        if(get_field('projektauswahl')) {
-            $projekte = get_field('projektauswahl');
-        }
-        if($projekte):foreach($projekte as $projekt):?>
+    <?php
+$projekte = get_posts(array(
+    'post_type' => "projekt",
+    'posts_per_page' => -1,
+));
+if (get_field('projektauswahl')) {
+    $projekte = get_field('projektauswahl');
+}
+if ($projekte): foreach ($projekte as $projekt): ?>
 
 
-        <a target="_blank" href="<?php echo get_field('externer_link', $projekt->ID) ? get_field('externer_link', $projekt->ID):get_permalink($projekt->ID);?>"class="projekt">
-            <div class="bild" style="background-image:url(<?php echo get_field('bild', $projekt->ID);?>"></div>
-            <div class="content">
-                <div style="color:<?php echo getProjectColor($projekt->ID);?>" class="subline">Projekt</div>
-                <div class="headline"><?php echo $projekt->post_title;?></div>
-                <div class="description"><?php the_field('kurzbeschreibung', $projekt->ID);?></div>
-                <span style="background:<?php echo getProjectColor($projekt->ID);?>" class="link"></span>
-            </div>
-        </a>
-        <?php endforeach;endif;
-    ?>
+	        <a target="<?php the_field('linkziel');?>" href="<?php echo get_field('externer_link', $projekt->ID) ? get_field('externer_link', $projekt->ID) : get_permalink($projekt->ID); ?>"class="projekt">
+	            <div class="bild" style="background-image:url(<?php echo get_field('bild', $projekt->ID); ?>"></div>
+	            <div class="content">
+	                <div style="color:<?php echo getProjectColor($projekt->ID); ?>" class="subline">Projekt</div>
+	                <div class="headline"><?php echo $projekt->post_title; ?></div>
+	                <div class="description"><?php the_field('kurzbeschreibung', $projekt->ID);?></div>
+	                <span style="background:<?php echo getProjectColor($projekt->ID); ?>" class="link"></span>
+	            </div>
+	        </a>
+	        <?php endforeach;endif;
+?>
 </section>
